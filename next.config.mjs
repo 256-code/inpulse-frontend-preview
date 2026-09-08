169d1eba25a61a364c428a9bd546545e27fe4c8c
256-code <panxing@libiaorobot.com>
2026-09-08 13:47:29 +0800

    ci: add github pages auto deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/inpulse-frontend-preview',
+  images: {
+    unoptimized: true,
+  },
   devIndicators: false,
 }
 
